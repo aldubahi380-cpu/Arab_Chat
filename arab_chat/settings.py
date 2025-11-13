bddae12a16fc7adbf81247a0c22a6d162ee6b9f3
aldubahi380-cpu <aldubahi380@gmail.com>
2025-11-13 22:14:42 +0300

    تم تمكين django.contrib.humanize داخل INSTALLED_APPS لإصلاح خطأ TemplateSyntaxError عند تحميل صفحة الاستوريات.

--- arab_chat/settings.py
+++ arab_chat/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.humanize',
     # Third party apps
     'rest_framework',
     'rest_framework.authtoken',
